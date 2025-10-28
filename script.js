143533096cabdf93bdb11d75a01e331817aa2b10
Mahmoud Seif <2305332@anu.edu.eg>
2025-10-28 15:56:10 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function chooseActor(movieId, castText) {
-  const span = document.getElementById(`${movieId}-recast`);
-  span.textContent = castText;
-  span.style.color = "#00ffb3"; // highlight color when user chooses
-}
